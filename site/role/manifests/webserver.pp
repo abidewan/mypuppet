e0321cf1ed31838c1c508e799247aa453627e666
Abi Dewan <abidewan7@gmail.com>
2016-06-10 14:27:45 +1000

    fixed the webserver.pp file

--- site/role/manifests/webserver.pp
+++ site/role/manifests/webserver.pp
@@ -1,10 +1,3 @@
 class role::webserver {
-<<<<<<< HEAD
-	#include profile::base
 	include profile::apache
 }
-=======
-	include profile::apache
-}
-
->>>>>>> b40d469ec80ae8d6bef968bd11b5bff1913c9125
